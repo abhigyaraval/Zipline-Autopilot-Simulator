06809ef3bdf64cabb5804be314878b7c4d97ca55
Abhigya Raval <abhigyaraval@hotmail.com>
2021-05-14 07:15:59 -0700

    NA

--- testing/pub.py
+++ testing/pub.py
@@ -3,8 +3,12 @@ from subprocess import Popen, PIPE
 import subprocess
 # Note: This is acting as the autopilot
 
-# TELEMETRY_STRUCT = struct.Struct(">f")   # INCOMING
-COMMAND_STRUCT = struct.Struct(">f")         # OUTGOING
+TELEMETRY_STRUCT = struct.Struct("i")   # INCOMING
+COMMAND_STRUCT = struct.Struct("i")         # OUTGOING
 
 while 1:
-    print(COMMAND_STRUCT.pack(float(10)))
+    print(COMMAND_STRUCT.pack(int(10)))
+    # print("Hello!")
+    # var  = input()
+    # var = TELEMETRY_STRUCT.unpack(var)
+    # print(var)
--- testing/sub.py
+++ testing/sub.py
@@ -3,11 +3,33 @@ from subprocess import Popen, PIPE
 
 # Note: This is acting as the simulator
 
-COMMAND_STRUCT = struct.Struct(">f")         # INCOMING
-# TELEMETRY_STRUCT = struct.Struct(">f")   # OUTGOING
+COMMAND_STRUCT = struct.Struct("i")         # INCOMING
+TELEMETRY_STRUCT = struct.Struct("i")   # OUTGOING
 
 pilot = Popen(['python3', 'pub.py'], stdin=PIPE, stdout=PIPE)
-
-while 1:
+qA                              
+i = 1
+while i:
+    # print("Hello!")
+    # i+=1
+    # print(i)
     cmd = pilot.stdout.read(COMMAND_STRUCT.size)
-    var = COMMAND_STRUCT.unpack(cmd)
+    # pilot.stdin.write(TELEMETRY_STRUCT.pack(float(10)))
+    # pilot.stdin.flush()
+    # print(COMMAND_STRUCT.size)
+    cmd = COMMAND_STRUCT.unpack(cmd)
+    print(cmd)
+    pilot.stdout.flush()
+
+
+#
+# TELEMETRY_STRUCT = struct.Struct(">Hhffb31B")
+# COMMAND_STRUCT = struct.Struct(">fB3s")
+#
+# # pilot = Popen(['python3', 'pub.py'], stdin=PIPE, stdout=PIPE)
+#
+# while 1:
+#     cmd = pilot.stdout.read(COMMAND_STRUCT.size)
+#     var = COMM
+#
+#
