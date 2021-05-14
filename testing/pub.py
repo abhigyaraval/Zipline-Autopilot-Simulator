dadb491443e0cb219c18fd400d618051df53d37d
Abhigya Raval <abhigyaraval@hotmail.com>
2021-05-14 08:20:56 -0700

    Just commiting before branching out

--- testing/pub.py
+++ testing/pub.py
@@ -1,14 +1,16 @@
 import struct
-from subprocess import Popen, PIPE
-import subprocess
+# from subprocess import Popen, PIPE
+# import subprocess
+import sys
+
 # Note: This is acting as the autopilot
 
-TELEMETRY_STRUCT = struct.Struct("i")   # INCOMING
-COMMAND_STRUCT = struct.Struct("i")         # OUTGOING
+TELEMETRY_STRUCT = struct.Struct(">d")   # INCOMING
+COMMAND_STRUCT = struct.Struct(">d")         # OUTGOING
 
 while 1:
-    print(COMMAND_STRUCT.pack(int(10)))
-    # print("Hello!")
-    # var  = input()
-    # var = TELEMETRY_STRUCT.unpack(var)
-    # print(var)
+    incoming = sys.stdout.read(TELEMETRY_STRUCT.size)
+    incoming = TELEMETRY_STRUCT.unpack(incoming)
+
+    COMMAND_STRUCT.pack(incoming)
+    sys.stdin.write(COMMAND_STRUCT)
--- testing/sub.py
+++ testing/sub.py
@@ -1,27 +1,32 @@
 import struct
 from subprocess import Popen, PIPE
+import sys
 
 # Note: This is acting as the simulator
 
-COMMAND_STRUCT = struct.Struct("i")         # INCOMING
-TELEMETRY_STRUCT = struct.Struct("i")   # OUTGOING
-
+COMMAND_STRUCT = struct.Struct(">d")         # INCOMING
+TELEMETRY_STRUCT = struct.Struct(">d")   # OUTGOING
+# print(COMMAND_STRUCT.size)
 pilot = Popen(['python3', 'pub.py'], stdin=PIPE, stdout=PIPE)
-qA                              
+file = open("data.txt", "w")
 i = 1
-while i:
+while i < 1000:
     # print("Hello!")
     # i+=1
+
     # print(i)
+    pilot.stdin.write(TELEMETRY_STRUCT.pack(float(1.2+i)))
+    # pilot.stdin.write(TELEMETRY_STRUCT.pack(int(i)))
     cmd = pilot.stdout.read(COMMAND_STRUCT.size)
-    # pilot.stdin.write(TELEMETRY_STRUCT.pack(float(10)))
+    print(sys.getsizeof(cmd))
     # pilot.stdin.flush()
     # print(COMMAND_STRUCT.size)
     cmd = COMMAND_STRUCT.unpack(cmd)
-    print(cmd)
+    file.write(cmd)
     pilot.stdout.flush()
+    i += 1
 
-
+file.close()
 #
 # TELEMETRY_STRUCT = struct.Struct(">Hhffb31B")
 # COMMAND_STRUCT = struct.Struct(">fB3s")
